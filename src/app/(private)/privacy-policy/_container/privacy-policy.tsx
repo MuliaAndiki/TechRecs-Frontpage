e5d701c3a619a114ad4ec0c2e3f39c5bb9abf347
diki <muliaandiki@gmaiil.com>
2025-09-18 11:55:49 +0700

    fixed: eslint

--- src/app/(private)/privacy-policy/_container/privacy-policy.tsx
+++ src/app/(private)/privacy-policy/_container/privacy-policy.tsx
@@ -1,5 +1,3 @@
-import { Label } from '@radix-ui/react-dropdown-menu';
-
 import Container from '@/src/components/ui/container';
 import PrivacyPolicyHeroSection from '@/src/core/section/privacy-policy/privacy-policy-hero-section';
 
